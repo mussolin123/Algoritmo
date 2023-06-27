05a4750b464428ce69d15089ef706e26568e0093
Leticia Mussolin <50248825+mussolin123@users.noreply.github.com>
2023-06-27 19:39:14 -0300

    Recursividade: *

--- Main2.java
+++ Main2.java
@@ -1,15 +1,23 @@
-class Main2 {
-//decrementa com recursividade. Chama a função decrementa, e e executa o decremento que chamou ele;
-//por isso repete 5 --0 e 0 --5;                                                                     
-    public static void decrementa(int x){
-        System.out.println(x);
-        if (x>0) //no IF se for uma linha só não precisa abrir e fechar chaves
-        decrementa (x-1);
-        System.out.println(x);
-            }
-    
-public static void main (String[] args){
-        int x=5;
-        decrementa(x);
+class Main2{
+
+    //faz a sequência com *, semppre diminuindo com recursividade (esse deu cert)
+    public static void linhaCaractere(int x){
+        int i;
+        for (i=0;i<x;i++){
+            System.out.printf("*");
+        }
+
+        System.out.println();
     }
-}
+        public static void trianguloRec(int n){
+        if(n>0){
+            linhaCaractere(n);
+            trianguloRec(n-1);
+
+            //linhaCaractere(n); ao tirar o coment, fica recursivo
+        }
+    }
+    public static void main(String[] args) {
+    trianguloRec(10);
+        }
+    }
